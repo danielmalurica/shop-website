b690d1821e489d558d4137d4be1f3cfc6bdec948
danielmalurica <danielmalurica@gmail.com>
2023-03-19 19:57:37 +0200

    bug fix

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
 import "./category-preview.styles.scss";
 
--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,15 +1,10 @@
-import React, { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../context/categories.context";
-import ProductCard from "../../product-card/product-card.component";
-import CategoryPreview from "../../category-preview/category-preview.component";
+import React from "react";
 import "./shop.styles.scss";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import CategoryComponent from "../category/category.component";
 
 const ShopComponent = () => {
-  const { categories } = useContext(CategoriesContext);
-
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
